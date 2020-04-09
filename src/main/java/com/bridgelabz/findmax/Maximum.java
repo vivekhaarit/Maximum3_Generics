dd8dda68816d1526ebd707e503b074937ed36099
vivek kumar <vivekhaarit@gmail.com>
2020-04-09 12:27:04 +0530

    method names refractored

--- src/main/java/com/bridgelabz/findmax/Maximum.java
+++ src/main/java/com/bridgelabz/findmax/Maximum.java
@@ -2,7 +2,7 @@ package com.bridgelabz.findmax;
 
 public class Maximum {
 
-    public static int maximumNo(Integer a,Integer b,Integer c){
+    public static int largestInteger(Integer a,Integer b,Integer c){
         if((a.compareTo(b)>0) && (a.compareTo(c)>0))
             return a;
         else if((b.compareTo(a)>0) && (b.compareTo(c)>0))
--- src/test/java/com/bridgelabz/findmax/TestMaximum.java
+++ src/test/java/com/bridgelabz/findmax/TestMaximum.java
@@ -5,23 +5,23 @@ import org.junit.Test;
 
 public class TestMaximum {
     @Test
-    public void checkWhether_firstNo_isMax() {
+    public void checkWhether_firstNo_isMaxInteger() {
         Maximum maximum = new Maximum();
-        Integer max3= maximum.maximumNo(30,10,20);
+        Integer max3= maximum.largestInteger(30,10,20);
         Integer expectation = 30;
         Assert.assertEquals(expectation,max3);
     }
     @Test
-    public void checkWhether_secondNo_isMax() {
+    public void checkWhether_secondNo_isMaxInteger() {
         Maximum maximum = new Maximum();
-        Integer max2= maximum.maximumNo(20,30,10);
+        Integer max2= maximum.largestInteger(20,30,10);
         Integer expectation = 30;
         Assert.assertEquals(expectation,max2);
     }
     @Test
-    public void checkWhether_thirdNo_isMax() {
+    public void checkWhether_thirdNo_isMaxInteger() {
         Maximum maximum = new Maximum();
-        Integer max1= maximum.maximumNo(10,20,30);
+        Integer max1= maximum.largestInteger(10,20,30);
         Integer expectation = 30;
         Assert.assertEquals(expectation,max1);
     }
