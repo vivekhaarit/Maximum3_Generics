a44d7fda9ef8085638e21e7a3f21eb13be858c61
vivek kumar <vivekhaarit@gmail.com>
2020-04-09 12:52:20 +0530

    refractored all 3 methods to 1 generic method

--- src/main/java/com/bridgelabz/findmax/Maximum.java
+++ src/main/java/com/bridgelabz/findmax/Maximum.java
@@ -2,23 +2,7 @@ package com.bridgelabz.findmax;
 
 public class Maximum {
 
-    public static Integer largestInteger(Integer a,Integer b,Integer c){
-        if((a.compareTo(b)>0) && (a.compareTo(c)>0))
-            return a;
-        else if((b.compareTo(a)>0) && (b.compareTo(c)>0))
-            return b;
-        else
-            return c;
-    }
-    public static Float largestFloat(Float a,Float b,Float c){
-        if((a.compareTo(b)>0) && (a.compareTo(c)>0))
-            return a;
-        else if((b.compareTo(a)>0) && (b.compareTo(c)>0))
-            return b;
-        else
-            return c;
-    }
-    public static String maxString(String a,String b,String c){
+    public static <T extends Comparable<T>> T maximumValue(T a,T b,T c){
         if((a.compareTo(b)>0) && (a.compareTo(c)>0))
             return a;
         else if((b.compareTo(a)>0) && (b.compareTo(c)>0))
--- src/test/java/com/bridgelabz/findmax/TestMaximum.java
+++ src/test/java/com/bridgelabz/findmax/TestMaximum.java
@@ -7,42 +7,42 @@ public class TestMaximum {
     @Test
     public void checkWhether_firstNo_isMaxInteger() {
         Maximum maximum = new Maximum();
-        Integer max3= maximum.largestInteger(30,10,20);
+        Integer max3= maximum.maximumValue(30,10,20);
         Integer expectation = 30;
         Assert.assertEquals(expectation,max3);
     }
     @Test
     public void checkWhether_secondNo_isMaxInteger() {
         Maximum maximum = new Maximum();
-        Integer max2= maximum.largestInteger(20,30,10);
+        Integer max2= maximum.maximumValue(20,30,10);
         Integer expectation = 30;
         Assert.assertEquals(expectation,max2);
     }
     @Test
     public void checkWhether_thirdNo_isMaxInteger() {
         Maximum maximum = new Maximum();
-        Integer max1= maximum.largestInteger(10,20,30);
+        Integer max1= maximum.maximumValue(10,20,30);
         Integer expectation = 30;
         Assert.assertEquals(expectation,max1);
     }
     @Test
     public void checkWhether_firstNo_isMaxFloat() {
         Maximum maximum = new Maximum();
-        Float max3= maximum.largestFloat(30.1f,10.2f,20.5f);
+        Float max3= maximum.maximumValue(30.1f,10.2f,20.5f);
         Float expectation = 30.1f;
         Assert.assertEquals(expectation,max3);
     }
     @Test
     public void checkWhether_secondNo_isMaxFloat() {
         Maximum maximum = new Maximum();
-        Float max2= maximum.largestFloat(20.5f,30.1f,10.2f);
+        Float max2= maximum.maximumValue(20.5f,30.1f,10.2f);
         Float expectation = 30.1f;
         Assert.assertEquals(expectation,max2);
     }
     @Test
     public void checkWhether_thirdNo_isMaxFloat() {
         Maximum maximum = new Maximum();
-        Float max1= maximum.largestFloat(10.2f,20.5f,30.1f);
+        Float max1= maximum.maximumValue(10.2f,20.5f,30.1f);
         Float expectation = 30.1f;
         Assert.assertEquals(expectation,max1);
     }
@@ -50,21 +50,21 @@ public class TestMaximum {
     @Test
     public void checkWhether_firstString_isMaxString() {
         Maximum maximum = new Maximum();
-        String max3= maximum.maxString("Apple","Peach","Banana");
+        String max3= maximum.maximumValue("Apple","Peach","Banana");
         String expectation = "Peach";
         Assert.assertEquals(expectation,max3);
     }
     @Test
     public void checkWhether_secondString_isMaxString() {
         Maximum maximum = new Maximum();
-        String max2= maximum.maxString("Peach","Banana","Apple");
+        String max2= maximum.maximumValue("Peach","Banana","Apple");
         String expectation = "Peach";
         Assert.assertEquals(expectation,max2);
     }
     @Test
     public void checkWhether_thirdString_isMaxString() {
         Maximum maximum = new Maximum();
-        String max1= maximum.maxString("Banana","Apple","Peach");
+        String max1= maximum.maximumValue("Banana","Apple","Peach");
         String expectation = "Peach";
         Assert.assertEquals(expectation,max1);
     }
